011e4b831a501326faa6b279dda364b38a7f9a47
Matheus Mendes ITI <matheuscabraal3@gmail.com>
2023-03-10 17:12:24 -0300

    Adicionado DIRETIVA ngClass, juntamente com o pensamento.length > 256

--- Alura_Memotec/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ Alura_Memotec/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,16 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos = [
-    // {
-    //   conteudo: 'Passo informações para o componente filho',
-    //   autoria: 'Componente pai',
-    //   modelo: 'modelo3',
-    // },
-    // {
-    //   conteudo: 'Minha Propriedade é Decorada com @Input',
-    //   autoria: 'Componente Filho',
-    //   modelo: 'modelo3',
-    // }
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha Propriedade é Decorada com @Input',
+      autoria: 'Componente Filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      autoria: '',
+      modelo: 'modelo1',
+    }
+
   ];
 
   constructor() {}
--- Alura_Memotec/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ Alura_Memotec/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -3,20 +3,28 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
-  styleUrls: ['./pensamento.component.css']
+  styleUrls: ['./pensamento.component.css'],
 })
-
-export class PensamentoComponent  implements OnInit {
-
+export class PensamentoComponent implements OnInit {
   @Input() pensamento = {
-    conteudo: 'Editando em Angular',
+    conteudo: 'I Love Angular',
     autoria: 'Matheus M.',
-    modelo: 'modelo3'
-  }
+    modelo: 'modelo3',
+  };
 
   constructor() {}
 
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
+  }
+
+  // UTILIZANDO UM MÉTODO NO COMPONENTE > ngClass
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    //else{
+    return 'pensamento-p';
+    //}
   }
 }
